87c391e226070b30fc76928587ceebd09e143ac3
Kamal Soni <iamkamalsoni7@gmail.com>
2023-03-16 01:19:04 +1100

    imported userslice state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import cartReducer from './cartSlice'
+import userReducer from './userSlice'
 
 const rootReducer = combineReducers({
   cart: cartReducer,
+  user:userReducer,
 
 });
 
